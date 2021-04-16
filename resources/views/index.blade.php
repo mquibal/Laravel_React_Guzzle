cd189e699e26c9fc371346fd7bf950917588ab72
Mariel Quibal <marielquibal@gmail.com>
2021-04-16 08:37:38 +0800

    Added index view for react

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel | React</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        <div id='root'></div>
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
